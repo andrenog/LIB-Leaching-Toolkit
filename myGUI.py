a63f27b4cd720eb36c945cfceaf09c5f58b85bed
André <80684611+andrenog@users.noreply.github.com>
2024-11-28 15:59:29 +0000

    GUI skeleton

--- myGUI.py
+++ myGUI.py
@@ -1,34 +1,154 @@
 import customtkinter
 
-# Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_appearance_mode("System")
-
-# Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_appearance_mode("light")
 customtkinter.set_default_color_theme("blue")
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        self.geometry("750x500")
+        self.title("LIB Recycling Helper")
 
-        # Window/App Customization
-        self.geometry("500x250")
-        self.title("Starter Code Example")
-
-        # Grid Configuration
-        self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure(1, weight=1)
 
-        # Widgets
-        label = customtkinter.CTkLabel(self, 
-                                       text="Hello Blog World!",
-                                       font=("Arial", 25),
+        self.navigation = NavigationFrame(self, controller=self)
+        self.navigation.grid(row=0, column=0, sticky="nesw")
+
+        self.frame5 = Frame5(self, controller=self)
+        self.frame4 = Frame4(self, controller=self)
+        self.frame3 = Frame3(self, controller=self)
+        self.frame2 = Frame2(self, controller=self)
+        self.frame1 = Frame1(self, controller=self)
+
+        self.frame1.grid(row=0, column=1, sticky="nsew")
+        self.frame2.grid(row=0, column=1, sticky="nsew")
+        self.frame3.grid(row=0, column=1, sticky="nsew")
+        self.frame4.grid(row=0, column=1, sticky="nsew")
+        self.frame5.grid(row=0, column=1, sticky="nsew")
+
+# Setup navigation bar on the lefthand side of the window
+class NavigationFrame(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.configure(fg_color="grey76")
+
+        # Title
+        title = customtkinter.CTkLabel(self, 
+                                       text="LIB RECYCLING",
+                                       anchor="center",
+                                       font=("Helvetica", 18))
+        title.grid(row=0, column=0, padx=(20, 20),pady=(10,0))
+
+        # Add buttons to access the different steps
+        test1 = customtkinter.CTkButton(self, 
+                                        text="0. Options",
+                                        anchor="center",
+                                        command=self.one_event)
+        test1.grid(row=1, column=0, padx=(20, 20), pady=(10,10))
+
+        test2 = customtkinter.CTkButton(self, 
+                                        text="1. Inputs",
+                                        anchor="center",
+                                        command=self.two_event)
+        test2.grid(row=2, column=0, padx=(20, 20), pady=(10,10))
+
+        test3 = customtkinter.CTkButton(self, 
+                                        text="2. Predictions",
+                                        anchor="center",
+                                        command=self.three_event)
+        test3.grid(row=3, column=0, padx=(20, 20), pady=(10,10))
+
+        test4 = customtkinter.CTkButton(self, 
+                                        text="3. Eco analysis",
+                                        anchor="center",
+                                        command=self.four_event)
+        test4.grid(row=4, column=0, padx=(20, 20), pady=(10,10))
+
+        test5 = customtkinter.CTkButton(self, 
+                                        text="4. Summary",
+                                        anchor="center",
+                                        command=self.five_event)
+        test5.grid(row=5, column=0, padx=(20, 20), pady=(10,10))
+
+    # These fucntions are called when the buttons above are pressed
+    def one_event(self):
+        print("Frame 1 button clicked")
+        self.controller.frame1.tkraise()
+    def two_event(self):
+        print("Frame 2 button clicked")
+        self.controller.frame2.tkraise()
+    def three_event(self):
+        print("Frame 3 button clicked")
+        self.controller.frame3.tkraise()
+    def four_event(self):
+        print("Frame 4 button clicked")
+        self.controller.frame4.tkraise()
+    def five_event(self):
+        print("Frame 5 button clicked")
+        self.controller.frame5.tkraise()
+
+# Setup the righthand panels shown after clicking the buttons in the navbar
+class Frame1(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        title = customtkinter.CTkLabel(self, 
+                                       text="OPTIONS HERE", 
                                        anchor="center")
-        button = customtkinter.CTkButton(self, text="Click Me!")
+        title.grid(row=0, column=0)
 
-        # Placing Widgets on Grid
-        label.grid(row=0, column=0)
-        button.grid(row=1, column=0)
+class Frame2(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
 
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        title = customtkinter.CTkLabel(self, 
+                                       text="INPUTS HERE", 
+                                       anchor="center")
+        title.grid(row=0, column=0)
+
+class Frame3(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        title = customtkinter.CTkLabel(self, 
+                                       text="ML RESULTS HERE", 
+                                       anchor="center")
+        title.grid(row=0, column=0)
+
+class Frame4(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        title = customtkinter.CTkLabel(self, 
+                                       text="ECO RESULTS HERE", 
+                                       anchor="center")
+        title.grid(row=0, column=0)
+
+class Frame5(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        title = customtkinter.CTkLabel(self, 
+                                       text="SUMMARY/EXPORT HERE", 
+                                       anchor="center")
+        title.grid(row=0, column=0)
+              
 app = App()
 app.mainloop()
