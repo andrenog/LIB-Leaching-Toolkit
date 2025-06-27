Fix metal leaching calculation
Refactored the metal leaching calculation in calcCosts() to compute the total mass of metal leached based on input composition and molar masses, improving cost normalization.

LIBtoolkit.py
@@ -34,7 +34,7 @@ class App(ctk.CTk):
         super().__init__()
         self.geometry("960x600")
         self.title("LIB Leaching Toolkit")
-        self.iconbitmap(getPath("icon.ico"))
+        self.iconbitmap(getPath("data/icon.ico"))
 
         self.grid_rowconfigure(1, weight=0)
         self.grid_rowconfigure(1, weight=1)
@@ -1621,7 +1621,28 @@ def calcCosts():
 
     # Normalize costs by the total amount of metal leached
     molar_masses = {'Li': 6.94, 'Ni': 58.69, 'Mn': 54.94, 'Co': 58.93}
-    total_metal_leached = (y_pred_mu * molar_masses).sum(axis=1) / 1000  # kg of metal leached
+
+    # Build the composition matrix: first column is 1s (Li), then inputNi, inputMn, inputCo
+    composition = np.column_stack([
+        np.ones(X_test.shape[0]),
+        X_test[['inputNi', 'inputMn', 'inputCo']].to_numpy()
+    ])
+
+    # Calculate the molar mass of the NMC material
+    # (in g/mol) based on the input composition and molar masses
+    NMC_molar_mass = (composition * list(molar_masses.values())).sum(axis=1) + 2*15.999
+
+    # Calculate the maximum mol of each metal that can be leached
+    # Total mols of NMC = mass of NMC (kg) * 1e3 (g/kg) / molar mass (g/mol)
+    max_metal = (mLIB * 1e3 / NMC_molar_mass)[:, None] * composition
+
+    # Calculate the mols leached for each metal
+    metals_leached = max_metal * y_pred_mu.to_numpy()
+    # Convert to mass in kg
+    metals_leached_kg = metals_leached * np.array(list(molar_masses.values())).reshape(1, -1) / 1000  # kg of metal leached
+
+    # Calculate the mass of each metal leached
+    total_metal_leached = metals_leached_kg.sum(axis=1)
 
     costs['Acid Cost, €/kg'] = costs['Acid Cost, €'] / total_metal_leached
     costs['Mixing, €/kg'] = costs['Mixing, €'] / total_metal_leached